22529b48a4f2e009f56ff13ac2b6f04bd821b85e
Tejasvi Vishwanadha <tvishwanadha@gmail.com>
2014-03-12 16:31:37 -0500

    initial commit, basic structure

---
+++ setup.py
@@ -0,0 +1,31 @@
+import os
+from setuptools import setup
+
+# Utility function to read the README file.
+# Used for the long_description.  It's nice, because now 1) we have a top level
+# README file and 2) it's easier to type in the README file than to put a raw
+# string in below ...
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name = "pybeats",
+    version = "0.0.1",
+    author = "Teja Vishwanadha",
+    author_email = "teja@beatsmusic.com",
+    description = ("Beats Music SDK"),
+    license = "BSD",
+    keywords = "daisy mog beats beatsmusic sdk",
+    url = "http://developer.beatsmusic.com",
+    packages=[
+        'pybeats'
+    ],
+    install_requires=[
+        "requests >= 2.3.0"
+    ],
+    long_description=read('README'),
+    classifiers=[
+        "Development Status :: 2 - Pre-Alpha",
+        "License :: OSI Approved :: BSD License"
+    ],
+)
